90b18c65e4358b8ab1df4f0e6d2a90f6f19609e9
mr_str <80831520+motorin-romchik@users.noreply.github.com>
2022-02-28 21:13:29 +0700

    Initial commit

---
+++ src/Ragged.java
@@ -0,0 +1,29 @@
+public class Ragged {
+    public static void main (String args[]){
+        int riders[][] = new int [7][];
+        riders [0] = new int [10];
+        riders [1] = new int [10];
+        riders [2] = new int [10];
+        riders [3] = new int [10];
+        riders [4] = new int [10];
+        riders [5] = new int [2];
+        riders [6] = new int [2];
+
+        for (int i = 0;i<5;i++){
+            for (int j = 0;j< 10;j++){
+                riders[i][j] = i+j+10;
+            }
+        }
+        for (int i =5;i<7;i++)
+            for (int j = 0;j<2;j++){
+                riders[i][j] = i+j+10;
+            }
+        System.out.println("Количество пассажиров, перевезенных каждым" +
+                " рейсом, в будние дни недели: ");
+        for (int i = 0; i <5;i++)
+            for (int j = 0;j<10;j++)
+                System.out.print(riders[i][j] + " ");
+        System.out.println();
+
+    }
+}
